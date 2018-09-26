b36357b07f2b92434379b1198cf6f49d615f6215
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-26 10:28:47 -0400

    the logic in implemented

--- WBE_FindFirstDuplicateChar/Program.cs
+++ WBE_FindFirstDuplicateChar/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/**
+ * Write a function that accepts a single string, and returns the first duplicated character in the string.
+ * ie: 'abcdc' should return 'c', and 'abcdbc' should return 'b'
+ */
+ 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +15,42 @@ namespace WBE_FindFirstDuplicateChar
     {
         static void Main(string[] args)
         {
+            do
+            {
+                try
+                {
+                    Console.Write("Enter a string of characters\n\n>>> ");
+                    string input = Console.ReadLine();
+                    Console.WriteLine(FindFirstDuplicatedChar(input));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.Write("\nPress Enter to try another string...");
+                Console.ReadLine();
+                Console.Clear();
+            } while (true);
+        }
+
+        static char FindFirstDuplicatedChar(string input)
+        {
+            int duplicateIndex = -1;
+            char[] array = input.ToCharArray();
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        if (duplicateIndex == -1 || j < duplicateIndex)
+                        {
+                            duplicateIndex = j;
+                        }
+                    }
+                }
+            }
+            return array[duplicateIndex];
         }
     }
 }
